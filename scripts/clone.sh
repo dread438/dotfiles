03075546bcca28d05752b996d9960e8e02cd96ed
Dread <dread438@proton.me>
2025-01-27 11:00:05 +0200

    Limit depth to save time

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -4,6 +4,6 @@ mkdir ~/git && cd $_
 git clone --quiet https://github.com/dread438/dwm
 git clone --quiet https://github.com/dread438/st
 git clone --quiet https://github.com/dread438/slstatus
-git clone --quiet https://github.com/jeffreytse/zsh-vi-mode
-git clone --quiet https://github.com/zdharma-continuum/fast-syntax-highlighting
-git clone --quiet https://aur.archlinux.org/yay-bin.git
+git clone --quiet --depth 1 https://github.com/jeffreytse/zsh-vi-mode
+git clone --quiet --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting
+git clone --quiet --depth 1 https://aur.archlinux.org/yay-bin.git
